9bdb6d076f975ffc0b7b9e888b552103f6d9fa5a
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2026-05-02 01:07:02 +0530

    Add another origin

--- backEnd/index.js
+++ backEnd/index.js
@@ -24,7 +24,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://bazzar-ghar-4nf7.vercel.app"
+    "https://bazzar-ghar-4nf7.vercel.app",
+    "https://bazzar-ghar-w99l.vercel.app"
   ],
   credentials: true
 }));
